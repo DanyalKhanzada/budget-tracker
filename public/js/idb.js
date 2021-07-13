208c1130a7cf97684d2ec3f364efe2d17d63cacc
“Danyal <danyalkhanzada89@gmail.com>
2021-07-13 18:57:06 -0400

    resolved

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,14 +3,3 @@ let modal = document.querySelector('#modal');
 let modalText = document.querySelector('#modalText');
 let mask = document.querySelector('#mask');
 
-function showModal(isOnline) {
-    if (isOnline) {
-        modalText.textContent = 'All saved transactions have been completed!';
-    }
-    else {
-        modalText.textContent = 'Transaction recorded! All transactions will be saved when an internet connection is reestablished.';
-    }
-    modal.style.display = 'flex';
-    mask.style.display = 'block';
-}
-
