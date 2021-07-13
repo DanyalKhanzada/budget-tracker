b79f8924d22e0d6ebf1aec8be56ac4555f82b86a
“Danyal <danyalkhanzada89@gmail.com>
2021-07-13 18:59:37 -0400

    completed

--- public/js/idb.js
+++ public/js/idb.js
@@ -31,3 +31,66 @@ request.onupgradeneeded = function (event) {
 
     db.createObjectStore('new_funds', { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadFunds();
+    };
+};
+
+request.onerror = function (event) {
+    console.log((event.target.errorCode));
+};
+
+// on attempt to submit transaction with no internet
+function saveRecord(record) {
+    const transaction = db.transaction(['new_funds'], 'readwrite');
+
+    const fundsObjectStore = transaction.objectStore('new_funds');
+
+    fundsObjectStore.add(record);
+
+    showModal(false);
+};
+
+function uploadFunds() {
+    const transaction = db.transaction(['new_funds'], 'readwrite');
+
+    const fundsObjectStore = transaction.objectStore('new_funds');
+
+    const getAll = fundsObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            // might need /transaction/bulk?
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_funds'], 'readwrite');
+
+                    const fundsObjectStore = transaction.objectStore('new_funds');
+
+                    fundsObjectStore.clear();
+
+                    showModal(true);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    }
+};
+
+window.addEventListener('online', uploadFunds);
